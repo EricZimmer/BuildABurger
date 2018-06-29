d4cd2e01b0bc10e5c1867b537060002c0e44ad24
Eric Zimmer <zimmer.eric@gmail.com>
2018-06-29 15:14:03 -0700

    modified side drawer and added toggle

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxhoc from '../../hoc/Auxhoc';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-  <Auxhoc>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Auxhoc>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
+  sideDrawerCloseHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  }
+  render() {
+    return (
+      <Auxhoc>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer 
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerCloseHandler} />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Auxhoc>
+    );
+  }
+};
 
 
-export default layout;
+export default Layout;
